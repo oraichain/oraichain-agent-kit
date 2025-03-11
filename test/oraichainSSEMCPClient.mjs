75b3502ffb9c27d43ec7e3d424eb83351642f776
ducphamle2 <ducphamle212@gmail.com>
2025-03-11 16:42:05 -0700

    feat: add poc of sse mcp connection & readme

---
+++ test/oraichainSSEMCPClient.mjs
@@ -0,0 +1,34 @@
+import { SSEClientTransport } from "@modelcontextprotocol/sdk/client/sse.js";
+
+async function main() {
+  // const receivedMessages = [];
+  const transport = new SSEClientTransport(
+    new URL("http://localhost:8080/sse"),
+  );
+
+  // Listen for SSE events
+  transport.onmessage = (event) => {
+    console.log("Received SSE message:", event.result);
+  };
+
+  await transport.start();
+  console.log("transport started");
+
+  const testMessage = {
+    jsonrpc: "2.0",
+    id: "test-1",
+    method: "tools/call",
+    params: {
+      _meta: { progressToken: 0 },
+      name: "TOKEN_BALANCE_ACTION",
+      arguments: {
+        address: "orai1f5nyvnx5ks738d5ys7pwa0evc42v6ff043h6d2",
+        denom: "orai",
+      },
+    },
+  };
+
+  await transport.send(testMessage);
+}
+
+main();
